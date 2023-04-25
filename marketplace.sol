4be89777337182bcc5402d2161931e0b10068d0f
nathanhtkd <nathanhartojo@gmail.com>
2023-04-24 22:14:28 -0500

    added some code

--- marketplace.sol
+++ marketplace.sol
@@ -1,10 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Marketplace {
-    // state variables
+/*
+    How does an nft contract interact with the marketplace?
+*/
+
+// NFT standard is ERC721
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// reentrant guard 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    address payable public owner;  
     
-    // constructor
+    // Mapping of NFT token ID to its price
+    mapping(uint256 => uint256) public nftPrices;   
     
-    // functions
+    // Mapping of NFT token ID to its current owner
+    mapping(uint256 => address) public nftOwners;
+
+    // Mapping of NFT token ID to its current state (for sale or not)
+    mapping(uint256 => bool) public nftForSale;
+
+    uint256 public transactionFee;
+
+    function listItem(address nftAddress, uint256 tokenID, uint256 price) public {
+
+    }
 }
+    
