0364c1c61bd54d210a3594f1779b30e52bef5258
Evanwutx <evanwutx12345@gmail.com>
2023-04-30 17:29:05 -0500

    added listing, unlisting, buying, withdrawing proceeds

--- marketplace.sol
+++ marketplace.sol
@@ -11,21 +11,51 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard {
+
     address payable public owner;  
     
-    // Mapping of NFT token ID to its price
-    mapping(uint256 => uint256) public nftPrices;   
+    struct NFT {
+    address nftAddress;
+    uint256 tokenID;
+    address owner;
+    uint256 price;
+    }
     
     // Mapping of NFT token ID to its current owner
-    mapping(uint256 => address) public nftOwners;
-
-    // Mapping of NFT token ID to its current state (for sale or not)
-    mapping(uint256 => bool) public nftForSale;
-
+    mapping(uint256 => NFT) public nftListings;
+    mapping(uint256 => bool) public isListed;
+    mapping(address => uint256) private sellerProceeds;
     uint256 public transactionFee;
 
-    function listItem(address nftAddress, uint256 tokenID, uint256 price) public {
+    function listItem(address nftAddress, uint256 tokenID, uint256 price) public payable nonReentrant {
+        require(price > 0, 'Price needs to be greater than 0');
+        require(msg.value == transactionFee, 'Please include the transaction fee');
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), tokenID);
+        nftListings[tokenID] = NFT(nftAddress, tokenID, msg.sender, price); 
+        isListed[tokenID] = true;
+    }
 
+    function unlistItem(address nftAddress, uint256 tokenID) public {
+        require(isListed[tokenID] == true, 'Not listed');
+        require(nftListings[tokenID].owner == msg.sender, 'Not your nft');
+        IERC721(nftAddress).transferFrom(address(this), msg.sender, tokenID);
+        isListed[tokenID] = false;
+    }
+
+    function buyNft(address nftAddress, uint256 tokenID) public payable nonReentrant {
+        require(isListed[tokenID] == true, 'Not listed');
+        NFT storage nft = nftListings[tokenID];
+        require(msg.value == nft.price, "Not enough money");
+        sellerProceeds[nft.owner] += msg.value;
+        IERC721(nftAddress).transferFrom(address(this), msg.sender, nft.tokenID);
+        nft.owner = msg.sender;
+        isListed[tokenID] = false;
+    }
+
+    function withdrawProceeds() public {
+        uint256 b = sellerProceeds[msg.sender];
+        sellerProceeds[msg.sender] = 0;
+        payable(msg.sender).transfer(b);
     }
 }
     
