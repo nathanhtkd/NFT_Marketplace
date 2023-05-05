76e92b2109b32aaf7dd968fed0c4abea06bd575a
nathanhtkd <nathanhartojo@gmail.com>
2023-05-04 21:31:44 -0500

    necessary files

---
+++ hardhat.config.js
@@ -0,0 +1,60 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+const REPORT_GAS = process.env.REPORT_GAS || false
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+    },
+   
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Marketplace"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
+}
